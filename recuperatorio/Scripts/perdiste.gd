144ec0ef9f1c134cc9804ae91dc8e7a4336dea2b
martinlartintin <benitezmartineugenio@gmail.com>
2025-12-03 15:40:05 -0300

    Agregar contador de muertes en la pantalla de perdiste

--- recuperatorio/Scripts/perdiste.gd
+++ recuperatorio/Scripts/perdiste.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var death_label: Label = $VBoxContainer/Perdiste
+
+func _ready() -> void:
+	death_label.text = "Muertes: %s" % Global.death_count
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ENTER"):
+	if event.is_action_pressed("enter"):
 		get_tree().change_scene_to_file("res://Scenes/Menú.tscn")
